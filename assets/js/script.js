87069b0f16827099d178e69c1fe8aa1471fe8e97
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2023-11-04 14:12:06 +0000

    set up structure of the game

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let userScore = 0;
+let computerScore = 0;
+let rounds = 0;
+const maxRounds = 10;
+
+const buttons = document.querySelectorAll('.buttons button');
+const userScoreDisplay = document.getElementById('user-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const userChoiceDisplay = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const gameResultDisplay = document.getElementById('game-result');
+const roundNumberDisplay = document.getElementById('round-number');
+const gameWinnerDisplay = document.getElementById('game-winner-result');
+const newGameButton = document.getElementById('new-game');
